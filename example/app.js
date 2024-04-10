4798a7afb782550d561cb6f966c133e59d4ed233
deandliu <deandliu@tencent.com>
2024-04-10 11:53:15 +0800

    update

--- example/app.js
+++ example/app.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import o2c from './index.js'
+import o2c from '../src/index.js'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static('public'))
+app.use(express.static(path.resolve(__dirname,'./public')))
 
 app.post('/api/o2c', async function (req, res) {
   const code = req.body.code
